e02d1f5c6471a05e66d5c4b74e1a59f2b742071b
Mani Maran <89793526+CodingCat555@users.noreply.github.com>
2026-03-14 01:50:54 +0530

    Add files via upload

--- data/quotes.js
+++ data/quotes.js
@@ -13,5 +13,32 @@ var QUOTES_DATA = [
     "category": "grace",
     "image": "quotes/images/quote1.jpg",
     "isActive": "yes"
+  },
+  {
+    "serial": 2,
+    "en": "2 Father alone exists. Nothing else. Nobody else.",
+    "ta": "2 தந்தை மட்டுமே இருக்கிறார். வேறு எதுவும் இல்லை. வேறு யாரும் இல்லை.",
+    "source": "Yogi Ramsuratkumar",
+    "category": "grace",
+    "image": "quotes/images/quote1.jpg",
+    "isActive": "yes"
+  },
+  {
+    "serial": 3,
+    "en": "3 Father alone exists. Nothing else. Nobody else.",
+    "ta": "3 தந்தை மட்டுமே இருக்கிறார். வேறு எதுவும் இல்லை. வேறு யாரும் இல்லை.",
+    "source": "Yogi Ramsuratkumar",
+    "category": "grace",
+    "image": "quotes/images/quote1.jpg",
+    "isActive": "yes"
+  },
+  {
+    "serial": 4,
+    "en": "4 Father alone exists. Nothing else. Nobody else.",
+    "ta": "4 தந்தை மட்டுமே இருக்கிறார். வேறு எதுவும் இல்லை. வேறு யாரும் இல்லை.",
+    "source": "Yogi Ramsuratkumar",
+    "category": "grace",
+    "image": "quotes/images/quote1.jpg",
+    "isActive": "yes"
   }
 ];
